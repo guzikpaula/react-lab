9617be2b2e5c8e6b203d94ccbb8431100170b439
Paulina Guzik <guzikpaula@gmail.com>
2026-05-16 10:04:36 +0200

    message email

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import {useState} from "react";
+import './App.css';
+
 function App() {
-    let email = 'pguzik@student.agh.edu.pl';
+    const [email, setEmail] = useState('pguzik@student.agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            {message}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
+
+
+
 }
 
 export default App;
